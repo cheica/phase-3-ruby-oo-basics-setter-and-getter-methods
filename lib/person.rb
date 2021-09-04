246bd7fed3a4d6d50e924513a885cdf058487f06
Chelsea <chelsea.icaza10@hotmail.com>
2021-09-04 18:25:06 -0400

    commit tree e18439d1f588381a574dd41fb0693921254cb36c

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
 class Person
+    def name=(human)
+        @name= human 
+    end
+
+    def name 
+        @name
+    end
+
+    def job=(work)
+        @job= work
+    end
+
+    def job
+        @job
+    end
+
 
 end
